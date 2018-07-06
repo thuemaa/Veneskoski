ab9e9bf91f7d02448dee290dbfd0fecd86430b61
thuemaa <35839876+thuemaa@users.noreply.github.com>
2018-07-06 12:57:37 +0300

    extended link menu

--- static/js/front.js
+++ static/js/front.js
@@ -2,14 +2,42 @@ $(document).ready(function() {
 
     /*hide the navbar*/
     $(".nav_links_container").hide();
+    $("#ajankohtaista_div").hide();
+    $("#tapahtumat_div").hide();
     /*set the click function for nav bar*/
     $("#navbutton").click(function() {
-        toggleNav();
+        toggleElement(this.id);
+    });
+
+    $("#ajankohtaista_a").click(function() {
+        toggleElement(this.id);
+    });
+
+    $("#tapahtumat_a").click(function() {
+        toggleElement(this.id);
     });
 });
 
-/*hide responsive navbar on click*/
-function toggleNav() {
-    console.log("painettii nappia");
-    $(".nav_links_container").toggle(500);
+/*click functions for toggling navbar elements*/
+function toggleElement(id) {
+    console.log("painettii nappia " + id);
+    if (id == "navbutton") {
+        $("#ajankohtaista_div").hide();
+        $("#tapahtumat_div").hide();
+        $(".nav_links_container").toggle();
+
+    }
+    else if (id == "ajankohtaista_a") {
+        $("#tapahtumat_div").hide();
+        $("#ajankohtaista_div").toggle();
+        /*change the background of active div*/
+        $("#" + id).parent().class("active_link");
+        //$("#" + id).addClass("active_link");
+        console.log( $("#ajankohtaista_a").parent() );
+    }
+
+    else {
+        $("#ajankohtaista_div").hide();
+        $("#tapahtumat_div").toggle();
+    }
 }
