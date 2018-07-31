e42a7b5a1d6b9725d142250d90dc81fa88df66b1
thuemaa <35839876+thuemaa@users.noreply.github.com>
2018-07-31 19:11:07 +0300

    view all ajankohtaista

--- soikka/urls.py
+++ soikka/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('ajankohtaista/<int:ak_pk>/', views.ajankohtaista, name='ajankohtaista'),
+    path('ajankohtaista/', views.ajankohtaista, name='ajankohtaista'),
     path('tapahtumat/<int:tapahtuma_pk>/', views.tapahtumat, name='tapahtumat'),
+    path('tapahtumat/', views.tapahtumat, name='tapahtumat'),
 ]
